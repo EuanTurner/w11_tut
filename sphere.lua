a1a7d7e79749a2d882de0a4696ab25bc1a57b3a0
EuanTurner <140374577+EuanTurner@users.noreply.github.com>
2023-10-12 23:09:49 +1000

    Sphere geometry and vtk file

--- sphere.lua
+++ sphere.lua
@@ -1,4 +1,17 @@
--- Test 2
--- test 3
--- test 4
--- new test
+-- Generate the sphere geometry 
+R = 10e-3
+
+a = {x=0.0, y=0.0}
+b = {x=-R, y=0.0}
+c = {x=0.0, y=R}
+d = {{x=-1.5*R,y=0.0}, {x=-1.5*R,y=R}, {x=-R,y=2*R}, {x=0.0,y=3*R}}
+
+psurf = CoonsPatch:new{
+    north=Line:new{p0=d[#d], p1=c}, east=Arc:new{p0=b, p1=c, centre=a},
+    south=Line:new{p0=d[1], p1=b}, west=Bezier:new{points=d}   
+}
+
+ni = 124; nj=256
+grid = StructuredGrid:new{psurface=psurf, niv=ni+1, njv=nj+1}
+
+grid:write_to_vtk_file('sphere.vtk')
